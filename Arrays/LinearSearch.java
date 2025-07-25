7df24939ab710494244fd78c1d65adeaf17db6d2
VaibhaviKalase <kalasevaibhavi@gmail.com>
2025-07-25 12:36:24 +0530

    Updated

---
+++ Arrays/LinearSearch.java
@@ -0,0 +1,20 @@
+public class LinearSearch{
+    public static int findIndex (int arr[],int key){
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] == key){
+                return i;
+            }
+        }
+        return -1;
+    }
+    public static void main(String as[]){
+        int arr[] = {2,4,6,8,10,12,14};
+        int key = 10;
+        int index = findIndex(arr,key);
+        if(index == -1){
+            System.out.println("NOT FOUND");
+        }else{
+            System.out.println("Element is at index : "+index);
+        }
+    }
+}
