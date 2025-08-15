8fe1d7b43a99341e3e8fcc3f80fb43c913c227dd
VaibhaviKalase <kalasevaibhavi@gmail.com>
2025-08-16 02:10:50 +0530

    Updated

--- TwoD_Array/Problem3.java
+++ TwoD_Array/Problem3.java
@@ -1,5 +1,5 @@
-/*Question 3 : Write a program to Find Transpose of a Matrix. 
-What is Transpose? => Transpose of a matrix is the process of swapping the rows to columns.*/
+/* Question 3 : Write a program to Find Transpose of a Matrix. 
+What is Transpose? => Transpose of a matrix is the process of swapping the rows to columns. */
 
 public class Problem3 {
     public static void Solution(int arr[][]){
