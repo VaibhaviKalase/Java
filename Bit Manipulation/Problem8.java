394d2f9ab679c24a21d774345a69e1620d17fb06
VaibhaviKalase <kalasevaibhavi@gmail.com>
2025-08-20 23:22:48 +0530

    Updated

---
+++ Bit Manipulation/Problem8.java
@@ -0,0 +1,9 @@
+// Check if a number is power of 2
+public class Problem8 {
+    public static boolean isPowerOfTwo(int n){
+        return (n&(n-1))==0;
+    }
+    public static void main(String[] args) {
+        System.out.println(isPowerOfTwo(8));
+    }
+}
